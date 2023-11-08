9770e0b8a3c60abff78b887c8a7167d7691e29df
Alex Su <suziyue@gmail.com>
2023-11-08 12:21:11 -0800

    database connection and .env setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+require('dotenv').config()
+// const { createClient } = require("./lib/supabase")
 const app = express()
 const port = 5500
+
+const db = require('./db/index')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -15,3 +19,22 @@ app.get('/wow', (req, res) => {
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`)
 })
+
+// app.get("/auth/callback", async function (req, res) {
+//   const code = req.query.code
+//   const next = req.query.next ?? "/"
+
+//   if (code) {
+//     const supabase = createClient({ req, res })
+//     await supabase.auth.exchangeCodeForSession(code)
+//   }
+
+//   res.redirect(303, `/${next.slice(1)}`)
+// })
+
+// app.post("/hello-world", async function (req, res, next) {
+//     const { email, emailConfirm } = req.body
+    
+  
+//     const supabase = createClient({ req, res })
+// })
