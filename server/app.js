8d1904d9b730f1ca7c79e49142554f6d85510ec1
yashgoy41 <yashgoy41@gmail.com>
2023-11-30 00:14:50 -0800

    using auth.getUser() to check if user is authenticated

--- server/app.js
+++ server/app.js
@@ -4,13 +4,13 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const supabaseServer = require('./supabaseServer');
 const supabase = supabaseServer.supabase;
-// const jwtDecode = require("jwt-decode");
+
 
 const app = express();
 const PORT = 3001;
 const JWT_SECRET = 'sJc9VtqYvylDhMTN74TsJHjcZ3FJ8cCqRBF5HYciRVDusulNxFqsrCITUAUDDReu3z3ZkuFZwmYESHUYSASEXw==';
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Use cors middleware to enable CORS
 app.use(cors());
@@ -23,14 +23,13 @@ const authenticateToken = (req, res, next) => {
     return res.status(401).json({ error: 'Unauthorized - Token not provided' });
   }
 
-  jwt.verify(token, JWT_SECRET, (err, user) => {
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
       console.log('Forbidden - Invalid token');
       return res.status(403).json({ error: 'Forbidden - Invalid token' });
     }
-    const decoded = jwt.decode(token);
+    // const decoded = jwt.decode(token);
     req.user = decoded.sub;
-    console.log(req.user);
     next();
   });
 };
@@ -38,16 +37,23 @@ const authenticateToken = (req, res, next) => {
 // Apply the authentication middleware to the route
 app.post('/add-to-wishlist', authenticateToken, async (req, res) => {
   try {
-    console.log('Received request to add to wishlist:', req.body);
-    
+    const userId = req.user; // Assuming req.user contains the user ID
     const { item } = req.body;
-    console.log('Item:', item);
-  
-    // Perform Supabase update based on userId
-    console.log(req.user.id);
+    // Check if user exists in the user table
+    const { data: userData, error: userError } = await supabase.auth.getUser(req.header('Authorization'))
+    if (userError || !userData) {
+      console.error('User not found:', userError);
+      return res.status(404).json({ error: 'User not found' });
+    }
+    // Check if user 'aud' is 'authenticated'
+    if (userData.user.aud !== 'authenticated') {
+      console.error('User is not authenticated');
+      return res.status(401).json({ error: 'User is not authenticated' });
+}
+    // Perform Supabase insert in wishlist table
     const { data, error } = await supabase
-    .from('wishlist')
-    .insert([{ userId: req.user, item, created_at: new Date() }]);
+      .from('wishlist')
+      .insert([{ userId: userId, item, created_at: new Date() }]);
 
     if (error) {
       console.error('Error updating Supabase data:', error);
@@ -55,13 +61,13 @@ app.post('/add-to-wishlist', authenticateToken, async (req, res) => {
     }
 
     console.log('Wishlist item added successfully:', data);
-
     res.json({ success: true, data });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
