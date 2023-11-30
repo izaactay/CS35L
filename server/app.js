de1c50b6b92e0c13c3bd40d5464fe474be76285f
yashgoy41 <yashgoy41@gmail.com>
2023-11-29 22:15:13 -0800

    initial auth draft

--- server/app.js
+++ server/app.js
@@ -1,40 +1,68 @@
-const express = require('express')
-require('dotenv').config()
-// const { createClient } = require("./lib/supabase")
-const app = express()
-const port = 5500
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const supabaseServer = require('./supabaseServer');
+const supabase = supabaseServer.supabase;
+// const jwtDecode = require("jwt-decode");
 
-const db = require('./db/index')
+const app = express();
+const PORT = 3001;
+const JWT_SECRET = 'sJc9VtqYvylDhMTN74TsJHjcZ3FJ8cCqRBF5HYciRVDusulNxFqsrCITUAUDDReu3z3ZkuFZwmYESHUYSASEXw==';
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(bodyParser.json());
 
-})
+// Use cors middleware to enable CORS
+app.use(cors());
 
-app.get('/wow', (req, res) => {
-    res.send('Wow! Hello World!')
+// Middleware to verify the JWT token
+const authenticateToken = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) {
+    console.log('Unauthorized - Token not provided');
+    return res.status(401).json({ error: 'Unauthorized - Token not provided' });
+  }
 
-})
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      console.log('Forbidden - Invalid token');
+      return res.status(403).json({ error: 'Forbidden - Invalid token' });
+    }
+    const decoded = jwt.decode(token);
+    req.user = decoded.sub;
+    console.log(req.user);
+    next();
+  });
+};
 
-app.listen(port, ()=>{
-    console.log(`Example app listening on port ${port}`)
-})
-
-// app.get("/auth/callback", async function (req, res) {
-//   const code = req.query.code
-//   const next = req.query.next ?? "/"
-
-//   if (code) {
-//     const supabase = createClient({ req, res })
-//     await supabase.auth.exchangeCodeForSession(code)
-//   }
-
-//   res.redirect(303, `/${next.slice(1)}`)
-// })
-
-// app.post("/hello-world", async function (req, res, next) {
-//     const { email, emailConfirm } = req.body
+// Apply the authentication middleware to the route
+app.post('/add-to-wishlist', authenticateToken, async (req, res) => {
+  try {
+    console.log('Received request to add to wishlist:', req.body);
     
+    const { item } = req.body;
+    console.log('Item:', item);
   
-//     const supabase = createClient({ req, res })
-// })
+    // Perform Supabase update based on userId
+    console.log(req.user.id);
+    const { data, error } = await supabase
+    .from('wishlist')
+    .insert([{ userId: req.user, item, created_at: new Date() }]);
+
+    if (error) {
+      console.error('Error updating Supabase data:', error);
+      return res.status(500).json({ error: 'Error updating Supabase data' });
+    }
+
+    console.log('Wishlist item added successfully:', data);
+
+    res.json({ success: true, data });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ server/supabaseServer.js
@@ -0,0 +1,9 @@
+// supabaseServer.js
+const { createClient } = require('@supabase/supabase-js');
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+    "https://pwtyqtdrwfzvtyfpfbpz.supabase.co", 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3dHlxdGRyd2Z6dnR5ZnBmYnB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAxNzA0NjMsImV4cCI6MjAxNTc0NjQ2M30.CLxdPSY9-tf2vC7m_4BuaWy4IHRwDOQub5b7-U7gj_4",
+);
+
+module.exports = { supabase };
