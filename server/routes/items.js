0f5e23a5bd8c50618fbcfa1d7a5233ab6a8bc8bd
Izaac Tay <izaac@g.ucla.edu>
2023-12-03 18:04:29 -0800

    Added items endpoints

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,18 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
+const { supabase } = require('../modules/supabaseServer');
 // middleware
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    data: 'get items'
-  });
+router.get('/:itemID', async (req, res) => {
+
+  const itemID = req.params.itemID;
+  const { data, error } = await supabase.from('Items').select().eq('id',itemID);
+  
+  if (error) {
+    console.error('Error reading Supabase data:', error);
+    return res.status(500).json({ error: 'Error reading Supabase data' });
+  }
+  else {
+    res.status(200).json({
+      data
+    });
+  }
 });
 
-router.post('/', (req, res) => {
-  res.status(200).json({
-    data: 'post items'
+
+router.get('/search/:query', async (req, res) => {
+
+  const search_query = req.params.query;
+  
+
+    
+  const{ data, error } = await supabase.from('Items').select().textSearch('name',search_query, {
+    type: 'websearch',
+    config: 'english'
   });
+
+  if (error) {
+    console.error('Error searching Supabase data:', error);
+    
+    
+    return res.status(501).json({ error: 'Error searching Supabase data' });
+    
+  }
+  else {
+    res.status(200).json({
+      data
+    });
+  }
+
+  
+
+  
 });
 
 module.exports = router;
