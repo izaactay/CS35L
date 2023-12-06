e6b5b151108aeb3ab673d6531c3879722647bf53
Izaac Tay <izaac@g.ucla.edu>
2023-12-05 20:49:33 -0800

    Added logout button

--- client/src/components/Login-Signup.js
+++ client/src/components/Login-Signup.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { Button } from '@mui/material';
+import {Typography, Container} from '@mui/material';
+
 
 
 //import "./Login-Signup.css";
@@ -44,27 +47,34 @@ function LoginBox(props) {
     return (
     
       <div className="login-container">
-        <h2>Hello {props.session.user.user_metadata.full_name}, Welcome Back!</h2>
+        <Container sx={{ marginTop: 4 }}>
         
+        <Typography>Hello {props.session.user.user_metadata.full_name}, Welcome Back!</Typography>
+          <Button block onClick={() => props.supabase.auth.signOut()}>
+            Sign out
+          </Button>
+          </Container>
         
       </div>
     );
   } else {
     return (
+      <Container sx={{ marginTop: 4 }}>
+        <div className="login-container">
+          <h2>Hello! Log In!</h2>
+          {error && <p className="error-message">Login failed. Please try again.</p>}
     
-      <div className="login-container">
-        <h2>Hello! Log In!</h2>
-        {error && <p className="error-message">Login failed. Please try again.</p>}
-  
-        <div className="alternative-signup">
-          <Auth
-            supabaseClient={props.supabase}
-            appearance={{ theme: ThemeSupa }}
-            providers={['google', 'github', 'facebook']}
-            view="sign_up"
-          />
+          <div className="alternative-signup">
+            <Auth
+              onlyThirdPartyProviders={true}
+              supabaseClient={props.supabase}
+              appearance={{ theme: ThemeSupa }}
+              providers={['google']}
+              view="sign_up"
+            />
+          </div>
         </div>
-      </div>
+      </Container>
     );
   }
   
